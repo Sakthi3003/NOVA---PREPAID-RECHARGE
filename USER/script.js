10d193e82957ee8a22f580823f32f3786cd23e41
Sakthi3003 <tsakthisowmya30@gmail.com>
2025-02-18 13:10:50 +0530

    updated file structure

--- USER/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    setTimeout(function() {
-        $("#splash-screen").fadeOut(300, function() {
-            $("#main-content").fadeIn(300);
-        });
-    }, 900);
-});
